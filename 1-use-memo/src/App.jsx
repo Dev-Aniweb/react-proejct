3e2f6e56e7d0c1cc50ce94884dad7cce1a371b09
Dev-Aniweb <anilkumarve51@gmail.com>
2026-01-08 23:23:03 +0530

    Updated changes

--- 1-use-memo/src/App.jsx
+++ 1-use-memo/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Assignment1 from './components/Assignment1'
 import Assignment2 from './components/Assignment2'
+import { Assignment3 } from './components/Assignment3'
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
     {/* <Assignment1 /> */}
-    <Assignment2 />
+    {/* <Assignment2 /> */}
+    <Assignment3/>
      
     </>
   )
--- 1-use-memo/src/components/Assignment2.jsx
+++ 1-use-memo/src/components/Assignment2.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 
 
 const words=["i","my", "name ", "is ", "anil","kumar ","verma ","wordpres developer "];
@@ -8,7 +8,7 @@ for (let i = 0; i < TOTAL_LINES; i++) {
   
     let sentence = "";
     for (let j = 0; j < words.length; j++) {
-         console.log(sentence);
+         
         sentence += (words[Math.floor(words.length * Math.random())])
         sentence += " "
     }
@@ -20,10 +20,29 @@ for (let i = 0; i < TOTAL_LINES; i++) {
 
 
 const Assignment2 = () => {
+  const[sentence,setSentence]=useState(ALL_WORDS);
+  const[filter,setFilter]=useState("")
+
+const filteredSentences = useMemo(()=>{
+   
+  return sentence.filter(x=>x.includes(filter));
+
+},[filter,sentence])
+ 
 
 
   return (
-    <div>Assignment2</div>
+    <div>
+      <input
+type='text'
+onChange={(e)=>{setFilter(e.target.value)}}
+/>
+{filteredSentences.map(word=><div> 
+  {word}
+
+</div>)}
+
+    </div>
   )
 }
 
