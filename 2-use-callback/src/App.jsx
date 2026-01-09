30b8bc951f9f982efefae62f3c292a5b5f11b529
Dev-Aniweb <anilkumarve51@gmail.com>
2026-01-09 12:24:06 +0530

    Added useCallback and useRef examples, updated project structure

---
+++ 2-use-callback/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Assignment1 from './components/Assignment1'
+import Assignment2 from './components/Assignment2'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Assignment1/> */}
+      <Assignment2/>
+      </div>
+  )
+}
+
+export default App
